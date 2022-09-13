de16d3e05282157cd67a8bc5ea82d3d44ed5e029
Jason Whisnant <tasups@gmail.com>
2022-09-13 14:07:36 -0400

    4th coding exercise completed with some help

---
+++ coding-exercises/4th-coding-exercise.js
@@ -0,0 +1,22 @@
+// Colt Steele Udemy JavaScript DSA Course, Coding Exercise 4
+
+function areThereDuplicates() {
+  let collection = {}
+  for (let val in arguments) {
+    collection[arguments[val]]
+      ? (collection[arguments[val]] += 1)
+      : (collection[arguments[val]] = 1);
+  }
+  for (let key in collection) {
+    if (collection[key] > 1) {
+      console.log(`There are duplicates in the arguments provided!`)
+      return true
+    }
+  }
+  console.log(`There are not duplicates in the arguments provided!`)
+  return false
+}
+
+areThereDuplicates(1, 2, 3) // false
+areThereDuplicates(1, 2, 2) // true 
+areThereDuplicates('a', 'b', 'c', 'a') // true 
