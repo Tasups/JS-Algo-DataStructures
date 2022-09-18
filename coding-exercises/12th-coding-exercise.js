972e48956aef5fa2d52286a8375f6747f8457b25
Jason Whisnant <tasups@gmail.com>
2022-09-17 20:06:05 -0400

    several exercises added

---
+++ coding-exercises/12th-coding-exercise.js
@@ -0,0 +1,16 @@
+// Colt Steele Udemy JavaScript DSA Course, Coding Exercise 12
+
+function productOfArray(arr) {
+  if (arr.length == 0) {
+    return 1
+  }
+  return arr[0] * productOfArray(arr.slice(1))
+}
+
+console.log(productOfArray([1,2,3])) // 6
+console.log(productOfArray([1,2,3,10])) // 60
+
+
+/**
+ * Write a function called productOfArray which takes in an array of numbers and returns the product of them all.
+ */
---
+++ coding-exercises/13th-coding-exercise.js
@@ -0,0 +1,13 @@
+// Colt Steele Udemy JavaScript DSA Course, Coding Exercise 13
+
+function recursiveRange(x) {
+  if (x === 0) return 0
+  return x + recursiveRange(x - 1)
+}
+
+console.log(recursiveRange(6)) // 21
+console.log(recursiveRange(10)) // 55 
+
+/**
+ * Write a function called recursiveRange which accepts a number and adds up all the numbers from 0 to the number passed to the function 
+ */
