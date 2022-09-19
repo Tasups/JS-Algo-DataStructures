ebb5086edf29244bc2c7321cee51fcc908d7418b
Jason Whisnant <tasups@gmail.com>
2022-09-19 13:53:52 +0000

    noted that the solution to 19 was out of place and will revisit

--- coding-exercises/19th-coding-exercise.js
+++ coding-exercises/19th-coding-exercise.js
@@ -2,6 +2,8 @@
 // I tried making this work, to no avail. I feel like I got close, however, when I look at Colt's
 //solution, I find that he capitalizes the entire word, not just the first letter!!! Bummer!
 
+//Come to find out that the solutions were not in the right order on Udemy! A whoopsie! will now attempt to correctly do the exercise later as I've got a sour taste in my mouth after working on this one for about 30 minutes!
+
 // MY ATTEMPT
 function capitalizeFirst(arr){
   let newArr = []
