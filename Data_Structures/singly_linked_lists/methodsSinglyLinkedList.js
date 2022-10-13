36323f37a79beb1093e007785012788cdf31b76d
Jason Whisnant <tasups@gmail.com>
2022-10-13 20:42:13 +0000

    added insert method to singly linked list

--- Data_Structures/singly_linked_lists/methodsSinglyLinkedList.js
+++ Data_Structures/singly_linked_lists/methodsSinglyLinkedList.js
@@ -92,12 +92,55 @@ class SinglyLinkedList {
     return this
   }
   
+  set(index, value){
+    let foundNode = this.get(index)
+    if(foundNode){
+      foundNode.val = value
+      return true
+    }
+    return false
+    // another way of doing it without using the get method
+    // if(index < 0 || index > this.length) return null
+    // let counter = 0
+    // let current = this.head
+    // while(counter !== index){
+    //   current = current.next
+    //   counter++
+    // }
+    // current.val = value
+    // return true
+  }
+  
+  get(index){
+    if(index < 0 || index > this.length) return null
+    let counter = 0
+    let current = this.head
+    while(counter < index){ // Colt Steele uses while(counter !== index), which is better?
+      current = current.next
+      counter++
+    }
+    return current
+  }
+  
+  insert(index, value){
+    if(index < 0 || index > this.length) return false
+    if(index === this.length) return !!this.push(value)
+    if(index === 0) return !!this.unshift(value)
+    
+    let newNode = new Node(value)
+    let prevNode = this.get(index - 1)
+    let temp = prevNode.next
+    prevNode.next = newNode
+    newNode.next = temp
+    this.length++
+    return true
+  }
+  
 }
 
 let list = new SinglyLinkedList();
-list.push("I want to go back");
-list.push("and woo those women");
-list.push("whom I missed.");
-list.unshift("One wish for me...")
-list.traverse()
+list.push("TWO");
+list.push("ONE");
+list.push("LIFTOFF!!!")
+console.log(list.insert(10,"GOBBLE!!! GOBBLE!!!"))
 console.log(list)
