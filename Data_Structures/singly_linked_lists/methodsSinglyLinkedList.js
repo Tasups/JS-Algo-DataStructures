8d656a55697f32d2e7accb339ffbdb0d3378c2c8
Jason Whisnant <tasups@gmail.com>
2022-10-16 17:06:50 -0400

    added reverse on singly linked lists

--- Data_Structures/singly_linked_lists/methodsSinglyLinkedList.js
+++ Data_Structures/singly_linked_lists/methodsSinglyLinkedList.js
@@ -66,18 +66,18 @@ class SinglyLinkedList {
     return oldHead
   }
   
-  push(val){
-    let newNode = new Node(val)
-    if (!this.head) {
-      this.head = newNode
-      this.tail = newNode // or this.tail = this.head
-    } else {
-      this.tail.next = newNode
-      this.tail = newNode
-    }
-    this.length += 1
-    return this
-  }
+  // push(val){
+  //   let newNode = new Node(val)
+  //   if (!this.head) {
+  //     this.head = newNode
+  //     this.tail = newNode // or this.tail = this.head
+  //   } else {
+  //     this.tail.next = newNode
+  //     this.tail = newNode
+  //   }
+  //   this.length += 1
+  //   return this
+  // }
   
   unshift(val){
     let newNode = new Node(val)
@@ -99,16 +99,6 @@ class SinglyLinkedList {
       return true
     }
     return false
-    // another way of doing it without using the get method
-    // if(index < 0 || index > this.length) return null
-    // let counter = 0
-    // let current = this.head
-    // while(counter !== index){
-    //   current = current.next
-    //   counter++
-    // }
-    // current.val = value
-    // return true
   }
   
   get(index){
@@ -123,9 +113,9 @@ class SinglyLinkedList {
   }
   
   insert(index, value){
-    if(index < 0 || index > this.length) return false
-    if(index === this.length) return !!this.push(value)
-    if(index === 0) return !!this.unshift(value)
+    if (index < 0 || index > this.length) return false
+    if (index === this.length) return !!this.push(value)
+    if (index === 0) return !!this.unshift(value)
     
     let newNode = new Node(value)
     let prevNode = this.get(index - 1)
@@ -135,12 +125,53 @@ class SinglyLinkedList {
     this.length++
     return true
   }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined
+    if (index === this.length - 1) return this.pop()
+    if (index === 0) return this.shift()
+    
+    let previousNode = this.get(index - 1)
+    let removedNode = previousNode.next
+    previousNode.next = removedNode.next
+    this.length--
+    console.log(removedNode)
+    return removedNode
+  }
+
+  reverse() {
+    let node = this.head
+    this.head = this.tail
+    this.tail = node;
+    let next
+    let prev = null
+    for (let i = 0; i < this.length; i++){
+      next = node.next
+      node.next = prev
+      prev = node
+      node = next
+    }
+    return this
+  }
+
+  print() {
+    let arr = []
+    let current = this.head
+    while (current) {
+      arr.push(current.val)
+      current = current.next
+    }
+    console.log(arr)
+  }
   
 }
 
 let list = new SinglyLinkedList();
-list.push("TWO");
 list.push("ONE");
-list.push("LIFTOFF!!!")
-console.log(list.insert(10,"GOBBLE!!! GOBBLE!!!"))
-console.log(list)
+list.push("TWO");
+list.push("THREE")
+list.traverse()
+list.reverse()
+list.traverse()
+console.log(list.head)
+console.log(list.tail)
