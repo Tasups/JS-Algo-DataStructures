9e566de5676d5c4d58220e0c9f86d628a1fb6452
Jason Whisnant <tasups@gmail.com>
2022-10-16 17:35:49 -0400

    started doubly linked list and push method

---
+++ Data_Structures/doubly_linked_lists/doublyLinkedList.js
@@ -0,0 +1,38 @@
+// DOUBLY LINKED LIST DATA STRUCTURE
+
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+    this.prev = null
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push(val) {
+    let newNode = new Node(val)
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+let list = new DoublyLinkedList()
+list.push("ONE")
+list.push("TWO");
+list.push("THREE");
+list.push("GO!!!");
+console.log(list)
