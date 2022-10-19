252da2f87352e4cb1727ceb383992cb03c3dc32a
Jason Whisnant <tasups@gmail.com>
2022-10-19 17:54:04 -0400

    added insert to trees

--- Data_Structures/trees/binarySearchTrees.js
+++ Data_Structures/trees/binarySearchTrees.js
@@ -11,6 +11,45 @@ class Node{
 class BinarySearchTree{
   constructor(){
     this.root = null
+  } 
+  insert(value) {
+    let newNode = new Node(value)
+    if (this.root === null) {
+      this.root = newNode
+      return this
+    } else {
+      let current = this.root
+      while (true) {
+        if (value === current.value) return undefined
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = newNode
+            return this
+          } else {
+            current = current.left
+          }
+        } else if (value > current.value) {
+          if (current.right === null) {
+            current.right = newNode
+            return this
+          } else {
+            current = current.right
+          }
+        }
+      }
+    }
   }
 }
+
+let BST = new BinarySearchTree()
+
+BST.insert(10)
+console.log(BST.insert(5));
+console.log(BST.insert(4));
+console.log(BST.insert(7));
+console.log(BST.insert(5));
+console.log(BST.insert(15));
+console.log(BST.insert(12));
+console.log(BST.insert(5));
+console.log(BST.insert(18));
 
