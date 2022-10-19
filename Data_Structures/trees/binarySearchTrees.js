36aa44fd1668c42e2e1999a2de105cd775d62dfd
Jason Whisnant <tasups@gmail.com>
2022-10-19 20:39:05 +0000

    starting trees

---
+++ Data_Structures/trees/binarySearchTrees.js
@@ -0,0 +1,16 @@
+// QUEUE DATA STRUCTURE
+
+class Node{
+  constructor(value){
+    this.value = value
+    this.left = null
+    this.right = null
+  }
+}
+
+class BinarySearchTree{
+  constructor(){
+    this.root = null
+  }
+}
+
