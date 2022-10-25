0b98778ba674b28f8ed8321b22672e426e51b681
Jason Whisnant <tasups@gmail.com>
2022-10-25 19:23:50 +0000

    priorityQueue added

--- Data_Structures/heaps/priorityQueue.js
+++ Data_Structures/heaps/priorityQueue.js
@@ -1,15 +1,16 @@
-// DATA HEAP STRUCTURE, FIRST THE MAX BINARY HEAP
+// PRIORITY QUEUE STRUCTURE, THIS IS VERY SIMILAR TO THE MAX BINARY HEAP BUT I WILL USE MIN BINARY HEAP
 // AS EXPRESSED IN AN ARRAY THE FIRST CHILD IS AT 2N + 1 AND THE SECOND AT 2N + 2
 // ALSO, AS EXPRESSED IN AN ARRAY, THE PARENT CAN BE FOUND BY THE FIRST CHILD AT MATH.FLOOR(N - 1 / 2), 
 // AND THE SECOND CHILD AT MATH.FLOOR(N - 2 / 2)
 
-class MaxBinaryHeap{
+class PriorityQueue{
   constructor(){
     this.values = []
   }
   
-  insert(element){
-    this.values.push(element)
+  enqueue(val, priority){
+    let newNode = new Node(val, priority)
+    this.values.push(newNode)
     this.bubbleUp()
   }
   
@@ -18,24 +19,24 @@ class MaxBinaryHeap{
     const element = this.values[idx]
     while(idx > 0){
       let parentIdx = Math.floor((idx - 1) / 2)
-      let parentVal = this.values[parentIdx]
-      if(element <= parentVal) break
+      let parent = this.values[parentIdx]
+      if(element.priority >= parent.priority) break
       this.values[parentIdx] = element
-      this.values[idx] = parentVal
+      this.values[idx] = parent
       idx = parentIdx
     }
     //console.log(this)
   }
   
-  extractMax(){
-    const max = this.values[0]
+  dequeue(){
+    const min = this.values[0]
     const end = this.values.pop()
     if(this.values.length > 0) {
       this.values[0] = end
       this.sinkDown()
     }
-    console.log(max)
-    return max
+    console.log(min)
+    return min
   }
   
   sinkDown(){
@@ -50,15 +51,15 @@ class MaxBinaryHeap{
       
       if(leftChildIdx < length){
         leftChild = this.values[leftChildIdx]
-        if(leftChild > element){
+        if(leftChild.priority < element.priority){
           swap = leftChildIdx
         }
       }
       if(rightChildIdx < length){
         rightChild = this.values[rightChildIdx]
         if(
-          (swap === null && rightChild > element) || 
-          (swap !== null && rightChild > leftChild)
+          (swap === null && rightChild.priority < element.priority) || 
+          (swap !== null && rightChild.priority < leftChild.priority)
           ){
           swap = rightChildIdx
         }
@@ -68,18 +69,34 @@ class MaxBinaryHeap{
       this.values[swap] = element
       idx = swap
     }
-    console.log(this)
   }
 }
-  
 
-let heap = new MaxBinaryHeap()
-heap.insert(41)
-heap.insert(39)
-heap.insert(33)
-heap.insert(18)
-heap.insert(27)
-heap.insert(12)
-heap.insert(55)
-heap.extractMax()
-heap.extractMax()
+class Node{
+  constructor(val, priority){
+    this.value = val
+    this.priority = priority
+  }
+}
+
+let ER = new PriorityQueue()
+ER.enqueue("common cold", 5)
+ER.enqueue("gunshot wound", 1)
+ER.enqueue("high fever", 4)
+ER.enqueue("broken arm", 2)
+ER.enqueue("glass in foot", 3)
+
+ER.dequeue()
+console.log(ER)
+console.log("*************************************************")
+ER.dequeue()
+console.log(ER)
+console.log("*************************************************")
+ER.dequeue()
+console.log(ER)
+console.log("*************************************************")
+ER.dequeue()
+console.log(ER)
+console.log("*************************************************")
+ER.dequeue()
+console.log(ER)
