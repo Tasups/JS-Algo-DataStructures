1880a1aeaf4373bdb676acd2447cb3f8353697ee
Jason Whisnant <tasups@gmail.com>
2022-10-29 17:39:10 -0400

    added addVertex method, started addEdge method

--- Data_Structures/graphs/undirectedGraph.js
+++ Data_Structures/graphs/undirectedGraph.js
@@ -4,5 +4,20 @@ class Graph{
   constructor(){
     this.adjacencyList = {}
   }
+
+  addVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+       this.adjacencyList[vertex] = [];
+    }
+  }
+
+  addEdge(v1, v2) {
+    
+  }
+
 }
 
+let g = new Graph()
+g.addVertex("Tokyo")
+g.addVertex("San Francisco")
+console.log(g)
