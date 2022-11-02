a523c4c0e2988983d0f690f1e305f1d481206882
Jason Whisnant <tasups@gmail.com>
2022-11-02 15:37:42 +0000

    removeVertex method added to graphs

--- Data_Structures/graphs/undirectedGraph.js
+++ Data_Structures/graphs/undirectedGraph.js
@@ -17,6 +17,7 @@ class Graph{
       this.adjacencyList[v1].push(v2)
       this.adjacencyList[v2].push(v1)
     }
+    console.log(undefined)
     return undefined
   }
   // this is for a directed graph, where you would only make an edge connect from one to another and 
@@ -25,6 +26,7 @@ class Graph{
     if(this.adjacencyList[v1] && this.adjacencyList[v2]){
       this.adjacencyList[v1].push(v2)
     }
+    console.log(undefined)
     return undefined
   }
   
@@ -33,23 +35,56 @@ class Graph{
       this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(v => v !== vertex2)
       this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(v => v !== vertex1)
     }
+    console.log(undefined)
+    return undefined
+  }
+  
+  removeVertex(vertex){
+    if(this.adjacencyList[vertex]){
+      // loop over every key in the object if it does not match the vertex in the args
+      while(this.adjacencyList[vertex].length){
+        console.log(vertex)
+        console.log(this.adjacencyList[vertex][0])
+        // you could use the pop method as well
+        this.removeEdge(vertex, this.adjacencyList[vertex][0])
+      }
+      delete this.adjacencyList[vertex]
+    }
+    console.log(undefined)
     return undefined
   }
 
 }
 
 let g = new Graph()
+/*
+  g.addVertex("Tokyo")
+  g.addVertex("San Francisco")
+  g.addVertex("Fresno")
+  console.log(g)
+  g.addEdge("Tokyo", "San Francisco")
+  console.log(g)
+  g.addDirectedEdge("Fresno", "San Francisco")
+  console.log(g.addDirectedEdge("Shelby", "San Francisco"))
+  console.log(g)
+  g.removeEdge("Tokyo", "San Francisco")
+  console.log(g)
+  g.removeEdge("San Francisco", "Fresno")
+  console.log(g)
+  console.log(g.removeEdge("Vienna", "Austria"))
+*/
+
+g.addVertex("Dallas")
 g.addVertex("Tokyo")
-g.addVertex("San Francisco")
-g.addVertex("Fresno")
+g.addVertex("Aspen")
+g.addVertex("Los Angeles")
+g.addVertex("Hong Kong")
+g.addEdge("Dallas", "Tokyo")
+g.addEdge("Dallas", "Aspen")
+g.addEdge("Hong Kong", "Tokyo")
+g.addEdge("Hong Kong", "Dallas")
+g.addEdge("Los Angeles", "Hong Kong")
+g.addEdge("Los Angeles", "Aspen")
 console.log(g)
-g.addEdge("Tokyo", "San Francisco")
+g.removeVertex("Shelby")
 console.log(g)
-g.addDirectedEdge("Fresno", "San Francisco")
-console.log(g.addDirectedEdge("Shelby", "San Francisco"))
-console.log(g)
-g.removeEdge("Tokyo", "San Francisco")
-console.log(g)
-g.removeEdge("San Francisco", "Fresno")
-console.log(g)
-console.log(g.removeEdge("Vienna", "Austria"))
