879e139aee3aa3a3e061fb77a16872a12391d37d
Jason Whisnant <tasups@gmail.com>
2022-12-05 20:40:17 +0000

    polished #1 fCC top 10

--- fCC-commoncodingquestions/valid-anagram.js
+++ fCC-commoncodingquestions/valid-anagram.js
@@ -12,10 +12,13 @@
 // the char shows up. This should be an O(n) time complexity as we're going through two loops, which is 
 // 2n, or n. There can be some shortening of the time by short-circuiting in the second loop if there is
 // no char found in the storage object. Else, I believe it is an O(n) and a space complexity of O(n).
+// hashtable for storage
 
 function validAnagram (str1, str2) {
  
   let charCollection = {}
+  
+  if(!str1.length === str2.length) return false
   
   for(let char of str1){
     if(charCollection[char]){
@@ -32,11 +35,24 @@ function validAnagram (str1, str2) {
       charCollection[char]--
     }
   }
+  // checks that if str1 has more of a char than str2
+  for(const [key,value] of Object.entries(charCollection)){
+    if(value !== 0){
+      console.log('close, but the two are not valid anagrams')
+      return false
+    }
+  }
   console.log(charCollection)
   return "the arguments are valid anagrams of one another"
 }
 
 console.log(validAnagram('tacocat', 'hot dog'))
+console.log("**************")
 console.log(validAnagram('tacocat', 'aaccott'))
-console.log()
+console.log("**************")
+console.log(validAnagram('danger', 'garden'))
+console.log("**************")
+console.log(validAnagram('TACOCAT', 'aaccott'))
+console.log("**************")
+console.log(validAnagram('tacoocat', 'aaccott'))
 
